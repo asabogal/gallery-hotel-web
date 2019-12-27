703c5d91c2aeb169e57248991089c281d59c2592
asabogal <asabogal.git@gmail.com>
2019-12-26 23:01:08 -0500

    initial folder structure and app layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Layout from './components/hoc/Layout';
+import Home from './pages/Home';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {  };
   }
+  
   render() {
     return (
-      <div>
-        
-      </div>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+           <Route exact path='/' component={Home}/>
+          </Switch>
+        </Layout>
+      </BrowserRouter>
     );
   }
 }
