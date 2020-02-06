35f5ac0899922b7312f5ccca47d99bb0b74b5f38
asabogal <asabogal.git@gmail.com>
2020-02-06 02:57:50 -0500

    init SocialBar

---
+++ src/components/utils/SocialBar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+const SocialBar = (props) => {
+  return (
+    <Container>
+      <a href="https://www.instagram.com/galleryhotelny/">
+        <FontAwesomeIcon 
+          icon={faInstagram} 
+          size={props.size}
+          color={props.color} 
+        />
+      </a>
+
+      <a href="https://www.facebook.com/galleryhotelny/">
+        <FontAwesomeIcon 
+          icon={faFacebookF} 
+          size={props.size}
+          color={props.color} 
+        />
+      </a>
+
+      <a href="mailto:info@galleryhotelny.com">
+        <FontAwesomeIcon 
+          icon={faEnvelope} 
+          size={props.size}
+          color={props.color} 
+        />
+        </a>
+     </Container>
+  );
+};
+
+export default SocialBar;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  a {
+    color: inherit;
+    padding: 15px 0px;
+    cursor: pointer;
+  }
+`;
