4e4e229914ab517ff10b9bb37179dd344c92fbc7
asabogal <asabogal.git@gmail.com>
2020-02-07 10:33:29 -0500

    add NavHeader and SideNav logic

--- src/components/hoc/Layout.js
+++ src/components/hoc/Layout.js
@@ -1,21 +1,32 @@
-import React from 'react';
-import styled from 'styled-components';
-import HeadMessage from '../header_footer/HeadMessage';
-import Header from '../header_footer/Header';
+import React, {useState} from 'react';
+import NavHeader from '../navigation/NavHeader';
+import SideNav from '../navigation/SideNav';
+import Backdrop from './Backdrop';
 
 const Layout = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSideNav = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeSideNav = () => {
+    setIsOpen(false);
+  };
+
+  let backdrop;
+  if (isOpen) {
+    backdrop = <Backdrop click={closeSideNav}/>
+  };
+
   return (
-    <Container>
-      <HeadMessage/>
-      <Header/>
+    <div>
+      <NavHeader toggleSideNav={toggleSideNav}/>
+      <SideNav open={isOpen} click={closeSideNav}/>
+      {backdrop}
       {props.children}
-    </Container>
+    </div>
   );
 };
 
 export default Layout;
-
-const Container = styled.div`
-  position: relative;
-  z-index: 999;
-`;
