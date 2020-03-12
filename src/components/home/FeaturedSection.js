d7d8ea9f86dd61d9da116d09a9d27e27f05796db
asabogal <asabogal.git@gmail.com>
2020-03-11 20:27:39 -0400

    add The Hotel, Rooms, Restauranat and Instagram sections

--- src/components/home/FeaturedSection.js
+++ src/components/home/FeaturedSection.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const FeaturedSection = ({title, description, links, image, side}) => {
+const FeaturedSection = ({title, subTitle, description, features, link, image, side}) => {
   return (
     <Wrapper side={side}>
       <TextContainer>
         <h1>{title}</h1>
+        { subTitle ? <h2>{subTitle}</h2> : null}
         <h5>
           {description}
         </h5>
         <ul>
+          { 
+            features ? 
+              Object.entries(features).map(([feature, icon]) => {
+                if (icon && typeof icon !== 'number') {
+                  return <Links key={feature}><img src={icon} alt='icon'/> {feature}</Links>
+                } else {
+                  return <Links key={feature}>{feature}</Links>
+                }
+              })
+            :
+              null 
+          }
+          <br></br>
           {
-            Object.entries(links).map(([link, icon]) => {
-              if (icon && typeof icon !== 'number') {
-                return <Links key={link}><img src={icon} alt='icon'/> {link}</Links>
-              } else {
-                return <Links key={link}><span>*</span> {link}</Links>
-              }
-            })
+            link ? 
+              <div>
+                <br></br>
+                <AnchorDiv>
+                 <Links><a href={link}> Visit {subTitle}</a> <span> &rarr;</span></Links>
+                </AnchorDiv>
+              </div>
+            :
+              null
           }
         </ul>
       </TextContainer>
@@ -34,16 +50,16 @@ const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   flex-direction: ${(props => props.side === 'right' ? 'row-reverse' : 'row')};
+  /* margin-top: 50px; */
 `;
 
 const TextContainer = styled.div`
-  flex: 0 1 35%;
+  flex: 0 1 40%;
   display: flex;
   flex-direction: column;
-  justify-self: center;
   justify-items: center;
-  height: auto;
-  margin: 20px;
+  /* height: auto; */
+  margin: 20px 30px;
   h1 {
     font-size: 44px;
     line-height: 58px;
@@ -63,22 +79,51 @@ const TextContainer = styled.div`
 `;
 
 const ImageContainer = styled.div`
+  position: relative;
   flex: 1 1;
-  width: 100%;
-  margin: 20px;
+  /* width: 60%; */
+  
+  height: auto;
   img {
+    position: absolute;
     width: 100%;
     height: auto;
-    min-height: 500px;
+    min-height: 80vh;
+    max-height: 650px;
   }
 `;
 
 const Links = styled.li`
+  display: flex;
+  align-items: center;
   font-size: 14px;
   list-style: none;
   margin-bottom: 15px;
   padding-bottom:5px;
-  width: 180px;
+  width: 220px;
   border-bottom: 1px solid rgb(0, 43, 92, 0.2);
-;
+  img {
+    padding-right: 10px;
+  }
+
+`;
+
+const AnchorDiv = styled.div`
+  cursor: pointer;
+  a {
+    font-size: 16px;
+    font-weight: 900;
+    text-decoration: none;
+    color: inherit;
+    padding-right: 10px;
+  }
+  span {
+    font-size: 20px;
+    transition: 0.5s;
+  }
+  &:hover {
+    span {
+      padding-left: 8px;
+    }
+  }
 `;
