3412bc6515689bc24e1c3a310223dffde39782be
asabogal <asabogal.git@gmail.com>
2020-03-11 22:09:24 -0400

    styled form

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -31,29 +31,31 @@ class ContactUs extends Component {
           <h1>CONTACT US</h1>
         </Heading>
         <div>
-          <form onSubmit={this.handleSubmit}>
-            <input
+          <Form onSubmit={this.handleSubmit}>
+            <Input
               placeholder='Name'
               type='text'
               name='name'
               value={this.state.name}
               onChange={this.handleChange}
             />
-            <input
+            <Input
               placeholder='Email'
               type='text'
               name='email'
               value={this.state.email}
               onChange={this.handleChange}
             />
-            <textarea
+            <TextArea
               placeholder='Message'
               name='message'
               value={this.state.message}
               onChange={this.handleChange}
             />
-            <Button type='submit'>SEND</Button>
-          </form>
+            <div style={{justifySelf: 'center'}}>
+             <Button type='submit'>SEND</Button>
+            </div>
+          </Form>
         </div>
       </Container>
     );
@@ -63,9 +65,44 @@ class ContactUs extends Component {
 export default ContactUs;
 
 const Container = styled.div`
-
+  display: grid;
+  width: 50vw;
+  margin: 0 auto;
+  padding-bottom: 20px;
+  margin-top: 10vh;
+  @media (max-width: 1000px) {
+  .contact-container {
+    width:  80vw;
+    }
+  }
 `;
 
 const Heading = styled.div`
+  text-align: center;
+  width: 100%;
+`;
 
+const Form = styled.form`
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: repeat(4, auto);
+  justify-content: center;
+`;
+
+const Input = styled.input`
+  border: 1px #002b5c solid;
+  height: 20px;
+  margin-bottom: 20px;
+  padding: .75rem .75rem;
+  font-size: 1rem;
+`;
+
+const TextArea = styled.textarea`
+  border: 1px #002b5c solid;
+  height: 9rem;
+  min-height: 9rem;
+  max-height: 12rem;
+  margin-bottom: 20px;
+  padding: .75rem .75rem;
+  font-size: 1rem;
 `;
