0e6eb92c63dd0f58ed0e4d3038a947235f5e20da
asabogal <asabogal.git@gmail.com>
2020-03-12 23:21:11 -0400

    add Picture component

--- src/components/utils/Pictures.js
+++ src/components/utils/Pictures.js
@@ -2,23 +2,36 @@ import React from 'react';
 import styled from 'styled-components'
 import {Link} from 'react-router-dom'
 
+
 export const Picture = (props) => {
+  return (
+   <Image src={props.picture} alt={props.alt}/>
+  )
+}
+
+export const Thumbnail = (props) => {
   if (props.link) {
     return (
       <Link to={props.link}>
-        <PictureContainer logo={props.logo} width={props.width} height={props.height} opacity={props.opacity}/>
+        <ThumbnailContainer picture={props.picture} width={props.width} height={props.height} opacity={props.opacity}/>
       </Link>
       )
   } else {
-    return <PictureContainer logo={props.logo} width={props.width} height={props.height} opacity={props.opacity}/>
+    return <ThumbnailContainer picture={props.picture} width={props.width} height={props.height} opacity={props.opacity}/>
   }
-
 }
 
-const PictureContainer = styled.div`
-  background: url(${props => props.logo}) no-repeat;
+const Image = styled.img`
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+`;
+
+const ThumbnailContainer = styled.div`
+  background: url(${props => props.picture}) no-repeat;
   background-size: cover !important;
-  background-position: start;
+  background-position: center center;
   width: ${props => props.width};
   height: ${props => props.height};
   opacity: ${props => props.opacity || '1'};
