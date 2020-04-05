9ae54d15c8f779e318e871a4ede9ee9a2be6c333
asabogal <asabogal.git@gmail.com>
2020-04-04 20:10:17 -0400

    init google map

---
+++ src/components/Map.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import {GoogleMap} from 'react-google-maps';
+
+const Map = () => {
+  return (
+    <div>
+      <GoogleMaap/>
+    </div>
+  );
+};
+
+export default Map;
