0ac5dad99abb633ef00dfdde6d34ff51b6bef6b7
asabogal <asabogal.git@gmail.com>
2020-04-04 23:12:18 -0400

    styled map + add marker

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
-import {GoogleMap, withScriptjs, withGoogleMap} from 'react-google-maps';
+import {GoogleMap, withScriptjs, withGoogleMap, Marker} from 'react-google-maps';
+import {mapStyle} from '../data/mapData/MapStyle';
+import hotel from '../images/location/hotel.svg';
+
 
 const Map = () => {
   return (
-    <GoogleMap defaultZoom={10} defaultCenter={{lat: 41.104491, lng: -72.360118}}/>
+    <GoogleMap 
+      defaultZoom={14} 
+      defaultCenter={{lat: 41.104491, lng: -72.360118}}
+      defaultOptions={{styles: mapStyle}}
+    >
+      <Marker 
+        position={{lat: 41.104491, lng: -72.360118}}
+        icon={hotel}
+      />
+    </GoogleMap>
   );
 };
 
