9c49926891a709ca9ce8506944bf4d1e0f8fe059
asabogal <asabogal.git@gmail.com>
2020-04-05 00:37:05 -0400

    init near by spots data

---
+++ src/data/mapData/NearbySpots.js
@@ -0,0 +1,83 @@
+export const spots = [
+  {
+    name: 'Brix & Rye',
+    description: 'A speakeasy bar specializing in Classic Cocktails and more modern variations. They serve top notch spirits, wines and beers in a low-key, casual setting.',
+    url: 'http://www.brixandrye.com',
+    coordinates: {
+      lat: 41.103945,
+      lng: 72.359591
+    }
+  },
+  {
+    name: '1943 Pizza Bar',
+    description: 'Little, relaxed eatery specializing in brick-oven pizza, salads & other Italian bites.',
+    url: 'https://rollingindoughpizza.com',
+    coordinates: {
+      lat: 41.104085,
+      lng: 72.359534
+    }
+  },
+  {
+    name: 'Stirling Sake',
+    description: 'Warm, minimal outpost offering Japanese sushi, sashimi & ramen noodles plus sake flights.',
+    url: 'http://stirlingsake.com',
+    coordinates: {
+      lat: 41.105058,
+      lgn: 72.360291
+    }
+  },
+  {
+    name: 'The North Fork Yoga Shala',
+    description: 'A full-service yoga studio featuring a broad schedule of classes to suit all ages and activity levels.',
+    url: 'https://northforkyogashala.com',
+    coordinates: {
+      lat: 41.104425,
+      lgn: 72.359532
+    }
+  },
+  {
+    name: 'The Times Vintage',
+    description: "A spacious shop is filled with one-of-a-kind pieces that are gently used & genuinely loved. Each item is hand selected & carefully curated based on it's uniqueness, the story it tells, & the nostalgia it evokes.",
+    url: 'https://www.timesvintage.com',
+    coordinates: {
+      lat: 41.104245,
+      lng: 72.360197
+    }
+  },
+  {
+    name: 'The Frisky Oyster',
+    description: 'Behind the modern facade is a creative American bistro boasting local ingredients & a hipster vibe.',
+    url: 'https://www.thefriskyoyster.com',
+    coordinates: {
+      lat: 41.102189,
+      lng: 72.359732
+    }
+  },
+  {
+    name: 'Lucharitos',
+    description: 'Playful eatery & bar serving Baja-style Mexican eats & tequila amid bright decor & artwork.',
+    url: 'https://www.lucharitos.com',
+    coordinates: {
+      lat: 41.102058,
+      lng: 72.359034
+    }
+  },
+  {
+    name: 'Little Creek Oyster Farm & Market',
+    description: 'Shucked oysters & local beers served in a snug harborside cottage with a market & outdoor seating.',
+    url: 'https://littlecreekoysters.com',
+    coordinates: {
+      lat: 41.101864,
+      lng: 72.359793
+    }
+  },
+  {
+    name: 'Bruce & Son',
+    description: 'Locally inspired breakfast & lunch menu, plus local tap beer & patio seating.',
+    url: 'http://bruceandsongreenport.com', 
+    coordinates: {
+      lat: 41.102718,
+      lng: 72.358966
+    }
+  }
+]
