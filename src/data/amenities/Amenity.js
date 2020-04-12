a297f5c68daa105dda367f46c971fb3ddf4ef510
asabogal <asabogal.git@gmail.com>
2020-04-12 15:02:38 -0400

    add amenities data

---
+++ src/data/amenities/Amenity.js
@@ -0,0 +1,46 @@
+export const amenities = [
+  {
+    heading: 'Our Garden',
+    description: "Our hidden gem. A beautiful, private garden for our guest to enjoy a relaxing time with ...."
+  },
+  {
+    heading: 'Our Lobby',
+    description: "The only hotel in Greenport with a full lobby...."
+  },
+  {
+    heading: 'Art Gallery',
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    heading: 'Movie and Media Room',
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    heading: 'Rooms',
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    heading: 'Suites',
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    heading: 'In-House Restaurant',
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    heading: 'High Speed Wi-Fi',
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    heading: 'Daily Housekeeping',
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    heading: 'Continental Breakfast',
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    heading: 'Private Parking',
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+]
