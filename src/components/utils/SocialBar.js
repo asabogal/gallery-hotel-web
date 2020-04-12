019f3032fc4c9b0f86764eb0b75fd219d7cc3ff1
asabogal <asabogal.git@gmail.com>
2020-04-11 21:49:03 -0400

    add Tripadvisor logo

--- src/components/utils/SocialBar.js
+++ src/components/utils/SocialBar.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faFacebookF} from '@fortawesome/free-brands-svg-icons';
+import { faTripadvisor} from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const SocialBar = (props) => {
@@ -19,6 +20,14 @@ const SocialBar = (props) => {
       <a href="https://www.facebook.com/galleryhotelny/" target="_blank" rel="noopener noreferrer">
         <FontAwesomeIcon 
           icon={faFacebookF} 
+          size={props.size}
+          color={props.color} 
+        />
+      </a>
+
+      <a href="https://www.tripadvisor.com/Hotel_Review-g47827-d8513453-Reviews-The_Gallery_Hotel-Greenport_North_Fork_Long_Island_New_York.html" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon 
+          icon={faTripadvisor} 
           size={props.size}
           color={props.color} 
         />
@@ -44,7 +53,7 @@ const Container = styled.div`
   align-items: center;
   a {
     color: inherit;
-    padding: 18px 0px;
+    padding: 10px 0px;
     cursor: pointer;
   }
 `;
