a05d98dff1aee3bd9a6419d52833fd6c3eaa2ae2
asabogal <asabogal.git@gmail.com>
2020-04-15 21:11:26 -0400

    change to functional component + pass new pages

--- src/App.js
+++ src/App.js
@@ -3,27 +3,27 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Layout from './components/hoc/Layout';
 import HomePage from './pages/HomePage';
 import Location from './pages/Location';
+import Rooms from './pages/Rooms';
+import RoomPage from './pages/RoomPage';
+import Amenities from './pages/Amenities';
 import ContactUs from './pages/ContactUs';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {  };
-  }
-  
-  render() {
-    return (
-      <BrowserRouter>
-        <Layout>
-          <Switch>
-              <Route exact path='/' component={HomePage}/>
-              <Route exact path='/location' component={Location}/>
-              <Route exact path='/contact' component={ContactUs}/>
-            </Switch>
-        </Layout>
-      </BrowserRouter>
-    );
-  }
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+            <Route exact path='/' component={HomePage}/>
+            <Route exact path='/location' component={Location}/>
+            <Route exact path='/amenities' component={Amenities}/>
+            <Route exact path='/rooms' component={Rooms}/>
+            <Route exact path='/contact' component={ContactUs}/>
+            <Route exact path= '/rooms/:name' render={props => <RoomPage {...props}/>}/>
+          </Switch>
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
