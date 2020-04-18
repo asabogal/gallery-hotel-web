618e8007e9d3ea55bbebc4a3bdbf2cbbb75c7911
asabogal <asabogal.git@gmail.com>
2020-04-18 00:35:22 -0400

    better layout + add responsiveness

--- src/components/navigation/NavHeader.js
+++ src/components/navigation/NavHeader.js
@@ -21,7 +21,7 @@ const NavHeader = (props) => {
       </LogoContainer>
       <ReserveContainer>
         <a href='https://clients.innroad.com/gallery/property.aspx'>
-          <Button width='160px'>BOOK A ROOM</Button>
+          <Button width='150px'>BOOK A ROOM</Button>
         </a>
       </ReserveContainer>
     </Container>
@@ -33,41 +33,57 @@ export default NavHeader;
 const Container = styled.div`
   position: sticky;
   top: 0;
-  left: 0;
   display: grid;
   opacity: ${props => props.scroll <=0 ? '1' : '0'};
   transition: 0.6s;
   background-color: white;
   grid-template-columns: 1fr 1fr 1fr;
-  justify-content: center;
+  grid-template-rows: 80px;
   align-items: center;
   z-index: 998;
   margin: 5px 0px;
+  padding: 0 40px;
+  @media (max-width: 599px) {
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 40px;
+    height: 40px;
+    align-items: flex-start;
+    p {
+      font-size: 12px;
+    }
+    button {
+      display: none;
+    }
+  }
 `;
 
 const MenuBtnContainer = styled.div`
-justify-self: start;
   display: flex;
   align-items: center;
-  margin-left: 50px;
+  justify-self: self-start;
   p {
-    margin-left: 10px;
+    margin-left: 20px;
   }
   
 `;
 
 const LogoContainer = styled.div`
   justify-self: center;
-  padding-right: 20px;
-  height: auto;
-  width: 200px;
+  width: auto;
+  height: 90%;
   img {
     width: 100%;
-    height: auto;
+    height: 70px;
   }
+  @media (max-width: 599px) {
+    justify-self: self-end;
+    img {
+      width: 100%;
+      height: 40px;
+    }
+  } 
 `;
 
 const ReserveContainer = styled.div`
-  justify-self: flex-end;
-  margin-right: 50px;
+  justify-self: self-end;
 `;
