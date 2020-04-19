b1116fcdc2b82e359353a946d9600a21d6200a78
asabogal <asabogal.git@gmail.com>
2020-04-19 00:21:02 -0400

    add move functionality

--- src/components/utils/ImageSlider.js
+++ src/components/utils/ImageSlider.js
@@ -1,33 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { Picture } from './Pictures';
 
 
 const ImageSlider = ({images}) => {
 
+  const [x, setX] = useState(0);
+
   const renderImages = () => {
     return images.map((image, index) => {
       return (
-        <Slide key={index}>
+        <Slide key={index} style={{transform: `translateX(${x}%)`}}>
           <img src={image}></img>
         </Slide>
       )
     })
-  }
+  };
+
+  const moveLeft = () => {
+    x === 0 ? setX(-100 * (images.length - 1)) : setX(x + 100);
+  };
+
+  const moveRight = () => {
+    x === -100 * (images.length - 1) ? setX(0) : setX(x - 100);
+  };
 
   return (
     <Slider>
       {renderImages()}
-      <LeftButton>
+      <LeftButton onClick={moveLeft}>
         <FontAwesomeIcon
           icon={faChevronLeft}
           size='2x'
           color='white'
         />
       </LeftButton>
-      <RightButton>
+      <RightButton onClick={moveRight}>
       <FontAwesomeIcon
         icon={faChevronRight}
         size='2x'
@@ -48,12 +59,15 @@ const Slider = styled.div`
   align-items: center;
   margin: 40px auto;
   position: relative;
+  overflow: hidden;
 `;
 
 const Slide = styled.div`
   border: 1px solid blue;
   min-width: 100%;
   height: 100%;
+  transition: 0.5s;
+  overflow: hidden;
   img {
     width: 100%;
     height: 100%;
