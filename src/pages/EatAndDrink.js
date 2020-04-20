7448274ba67b4b66ab4b9e0f5546fdab0a6f38c2
asabogal <asabogal.git@gmail.com>
2020-04-20 17:06:54 -0400

    init EatAndDrink page

---
+++ src/pages/EatAndDrink.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import HeroHeader from '../components/eat_drink/HeroHeader';
+
+// styled
+import {PageContainer} from '../components/utils/StyledLayouts';
+
+
+const EatAndDrink = () => {
+  return (
+    <div>
+      <HeroHeader/>
+      <PageContainer>
+        
+      </PageContainer>
+    </div>
+  );
+};
+
+export default EatAndDrink;
+
