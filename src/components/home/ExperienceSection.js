c7974f14398dee04c7c8267ca1c12d42c2fe08b1
asabogal <asabogal.git@gmail.com>
2020-04-20 01:38:04 -0400

    init ExperienceSection

---
+++ src/components/home/ExperienceSection.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Thumbnail} from '../utils/Pictures';
+// images
+import lounge from '../../images/home/experience/lounge.png';
+import garden from '../../images/home/experience/garden.png';
+
+const ExperienceSection = () => {
+  return (
+    <Container>
+      <ImageContainer>
+        <Thumbnail 
+          image={lounge} 
+          alt='lounge-image'
+          heading='Art Gallery'
+          info='More than a hotel... lorem ipsum dolor sit amet'
+          link='/amenities/gallery'
+          />
+      </ImageContainer>
+      <InfoContainer>
+        <TextContainer>
+          <h2>Experience</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </TextContainer>
+        <ImageContainer>
+          <Thumbnail 
+            image={garden} 
+            alt='garden-image'
+            heading='Garden'
+            info='Sit back, relax, enjoy a glass of rosé'
+            link='/amenities/garden'
+            />
+        </ImageContainer>
+      </InfoContainer>
+    </Container>
+  );
+};
+
+export default ExperienceSection;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 2rem;
+`;
+
+const ImageContainer = styled.div`
+  align-self: center;
+`;
+
+const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 10vh;
+`;
+
+const TextContainer = styled.div`
+`;
