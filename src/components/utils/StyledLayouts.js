d376eeecf2f8481a241be6afddca682c5cc6ec95
asabogal <asabogal.git@gmail.com>
2020-04-19 21:24:26 -0400

    add responsiveness

--- src/components/utils/StyledLayouts.js
+++ src/components/utils/StyledLayouts.js
@@ -2,5 +2,14 @@ import styled from 'styled-components';
 
 export const HeadingContainer = styled.div`
 margin-top: 80px;
+margin-bottom: 10vh;
 text-align: center;
+`;
+
+export const PageContainer = styled.div`
+  margin: 40px;
+  background-color: rgba(255, 255, 255, 0.3);
+  @media (max-width: 599px) {
+    margin: 5px;
+  }
 `;
