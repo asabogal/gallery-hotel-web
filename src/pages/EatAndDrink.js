cd7d09781b7d3e39d57062a9a439186414e36abe
asabogal <asabogal.git@gmail.com>
2020-04-21 17:22:46 -0400

    set EatAndDrink Page;

--- src/pages/EatAndDrink.js
+++ src/pages/EatAndDrink.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 import HeroHeader from '../components/eat_drink/HeroHeader';
-
+import InfoSection from '../components/eat_drink/InfoSection';
+import Plates from '../components/eat_drink/Plates';
+import Seating from '../components/eat_drink/Seating';
 // styled
 import {PageContainer} from '../components/utils/StyledLayouts';
-
 
 const EatAndDrink = () => {
   return (
     <div>
       <HeroHeader/>
       <PageContainer>
-        
+        <InfoSection/>
+        <Plates/>
+        <Seating/>
       </PageContainer>
     </div>
   );
