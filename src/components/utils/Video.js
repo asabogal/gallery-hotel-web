a5f90b464c6c0cb963a3789717cb450918f2bc61
asabogal <asabogal.git@gmail.com>
2020-04-23 22:12:21 -0400

    move Video to utilities

--- src/components/utils/Video.js
+++ src/components/utils/Video.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import styled from 'styled-components'
-import video from '../../videos/intro_video.mp4'
 
-const Video = () => {
+const Video = ({video}) => {
   return (
-    <VideoContainer>
+    <VideoContainer video={video} height={height} width={width}>
       <video autoPlay loop muted>
         <source src={video} type='video/mp4' />
       </video>
@@ -16,7 +15,6 @@ export default Video;
 
 const VideoContainer = styled.div`
  video {
-  width: 100vw;
   min-height: 32vh;
   max-height: 90vh;
   object-fit: cover;
