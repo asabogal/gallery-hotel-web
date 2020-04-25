c1007f390eb7c1b11b96dfc0905cd239dba416c8
asabogal <asabogal.git@gmail.com>
2020-04-25 01:36:07 -0400

    styled text and add cards

--- src/components/location/Text.js
+++ src/components/location/Text.js
@@ -1,28 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
-
-
-// icons
-import gps from '../../images/location/gps.svg';
-import phone from '../../images/location/phone.svg';
+import ArrivalCards from './ArrivalCards';
+import SpotCards from './SpotCards';
 
 const Text = () => {
   return (
     <Container>
-      <h1>Location</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <h1>Arrival</h1>
+      <h2>Arrival</h2>
       <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <InfoBox>
-        <br></br>
-        <h3>Find Us Here</h3>
-        <li>
-         <a href="https://goo.gl/maps/1VRrR5PhMrXCVvKm7" target="_blank" rel="noopener noreferrer"><img src={gps} alt='gps-icon'/>439 Main Street - Greenport, NY</a>
-        </li>
-        <li>
-          <a href="tel: +1-631-477-4000"><img src={phone} alt='phone-icon'/>631-477-4000</a>
-        </li> 
-      </InfoBox>
+      <ArrivalCards />
+      <h2>Local Spots</h2>
+      <p>Greenport is the North Fork's premier destination. Restaurants, shops and other area attractions are walking distance from the hotel. The east coast's best wineries and local farms are just a short drive away. Local transportation, designated driver service, and winery tours available.</p>
+      <h4>Recommended</h4>
+      <SpotCards />
     </Container>
   );
 };
@@ -30,20 +20,16 @@ const Text = () => {
 export default Text;
 
 const Container = styled.div`
-`;
-
-const InfoBox = styled.ul`
-  list-style: none;
-  li {
-    margin: 10px;
+  padding-top: 40px;
+  @media (max-width: 599px) {
+    flex-direction: column;
+    text-align: center;
   }
-  img {
-    padding-right: 5px;
+  h4 {
+    font-size: 18px;
+    margin-bottom: 5px;
+    font-weight: 400;
+    letter-spacing: 0px;
   }
-  a {
-    color: #002b5c;
-    text-decoration: none;
-  }
-
 `;
 
