a4e6cb3170c7aa994802999dfbe9a7aee253444f
asabogal <asabogal.git@gmail.com>
2020-04-25 01:38:40 -0400

    style and add components

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -2,19 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 import Text from '../components/location/Text';
 import GoogleMap from '../components/GoogleMap';
-
+// styled
+import {PageContainer} from '../components/utils/StyledLayouts';
+ 
 const Location = () => {
   return (
-    <Container>
-      <Text/>
-      <GoogleMap 
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `100%` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-        withSpots={true}
-        />
-    </Container>
+    <PageContainer>
+      <Container>
+        <Text/>
+        <GoogleMap 
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `100%` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          withSpots={true}
+          />
+      </Container>
+    </PageContainer>
   );
 };
 
@@ -24,6 +28,9 @@ const Container = styled.div`
   display: grid;
   gap: 2rem;
   grid-template-columns: 1fr 1fr;
-  grid-template-rows: auto;
-  margin: 40px;
+  @media (max-width: 1099px) {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: auto 50vh;
+  }
 `;
