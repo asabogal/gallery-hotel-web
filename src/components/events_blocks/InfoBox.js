821e92e1f750e50bb5dc6f6410db771289de73b1
asabogal <asabogal.git@gmail.com>
2020-04-25 14:40:11 -0400

    change description

--- src/components/events_blocks/InfoBox.js
+++ src/components/events_blocks/InfoBox.js
@@ -5,7 +5,8 @@ const InfoBox = () => {
   return (
     <Container>
       <h2>From intimate and private events, to entire hotel takeovers </h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p>We have multiple spaces to choose from for next event. Our gorgeous Garden is the ideal space for a delightful brunch or an indulgent sunset rosé hour. The Gallery and Lounge are ideal for a stylish and intimate evening cocktail reception. Our Media room can be transformed into a meeting space for your next corporate meeting. You may choose single spaces, or a combination of them for up to 100 guests.</p>
+      <p>Group reservations and wedding room blocks are available for partial or full hotel buyout. Contact us for more information.</p>
     </Container>
   );
 };
