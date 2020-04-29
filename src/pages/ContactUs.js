f3e271546c2213f0988f9c5c7358dffaf31241c8
asabogal <asabogal.git@gmail.com>
2020-04-29 19:47:42 -0400

    refactor as functional component + useReducer and useEffect

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,70 +1,72 @@
-import React, { Component } from 'react'
+import React, {useReducer, useEffect} from 'react'
 import styled from 'styled-components';
 import {Button} from '../components/utils/Buttons';
 
-class ContactUs extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
+const ContactUs = () => {
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  })
+
+  const [userInput, setUserInput] = useReducer((state, newState) => ({...state, ...newState}),
+    {
       name: '',
       email: '',
       message: ''
-     };
-  }
+    }  
+  );
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    alert('message sent');
-  }
-
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const {name, value} = e.target;
-    this.setState({
+    setUserInput({
       [name]: value
     });
   }
 
-  render() {
-    return (
-      <Container>
-        <Heading>
-          <h1>Contact Us</h1>
-          <p>
-            For immidiate assistance or to reach us directly, please call us at <a style={{color: '#002b5c'}}
-            href="tel: +1-631-477-4000"> 631-477-4000 </a>.<br></br>
-            For all other inquiries and requests, please fill in the form below.
-          </p>
-        </Heading>
-        <div>
-          <Form onSubmit={this.handleSubmit}>
-            <Input
-              placeholder='Name'
-              type='text'
-              name='name'
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-            <Input
-              placeholder='Email'
-              type='text'
-              name='email'
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-            <TextArea
-              placeholder='Message'
-              name='message'
-              value={this.state.message}
-              onChange={this.handleChange}
-            />
-            <div style={{justifySelf: 'center'}}>
-             <Button type='submit'>SEND</Button>
-            </div>
-          </Form>
-        </div>
-      </Container>
-    );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('message sent');
   }
+
+  return (
+    <Container>
+      <Heading>
+        <h1>Contact Us</h1>
+        <p>
+          For immidiate assistance or to reach us directly, please call us at <a style={{color: '#002b5c'}}
+          href="tel: +1-631-477-4000"> 631-477-4000 </a>.<br></br>
+          For all other inquiries and requests, please fill in the form below.
+        </p>
+      </Heading>
+      <div>
+        <Form onSubmit={handleSubmit}>
+          <Input
+            placeholder='Name'
+            type='text'
+            name='name'
+            value={userInput.name}
+            onChange={handleChange}
+          />
+          <Input
+            placeholder='Email'
+            type='text'
+            name='email'
+            value={userInput.email}
+            onChange={handleChange}
+          />
+          <TextArea
+            placeholder='Message'
+            name='message'
+            value={userInput.message}
+            onChange={handleChange}
+          />
+          <div style={{justifySelf: 'center'}}>
+            <Button type='submit'>SEND</Button>
+          </div>
+        </Form>
+      </div>
+    </Container>
+  );
 }
 
 export default ContactUs;
