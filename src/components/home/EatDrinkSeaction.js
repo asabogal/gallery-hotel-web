3b09213f55fbf9f986e45ac34461350a2955f787
asabogal <asabogal.git@gmail.com>
2020-07-30 20:52:50 -0400

    update description + fix img alt text + better layout

--- src/components/home/EatDrinkSeaction.js
+++ src/components/home/EatDrinkSeaction.js
@@ -1,32 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
+import {TwoColCaptionImage} from '../utils/StyledLayouts';
 import {Thumbnail} from '../utils/Pictures';
-import {Button} from '../utils/Buttons';
-import {Link} from 'react-router-dom';
 // images
-import signatureDrink from '../../images/home/restaurant/signaturedrink.png';
+import mojito from '../../images/home/restaurant/mojito.png';
 import friedRice from '../../images/home/restaurant/friedrice.png';
 import bar from '../../images/home/restaurant/bar.png';
 
 const EatDrinkSection = () => {
   return (
-    <Container>
-      <InfoContainer>
-        <TextContainer>
-          <h2>Eat & Drink</h2>
-          <p>Tropical Vibes, Eclectic Flavors,..........  consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        </TextContainer>
-        <InfoImages>
+    <TwoColCaptionImage height='90vh'>
+      <TextContainer>
+        <h2>Eat & Drink</h2>
+        <p>Tropical Vibes and Eclectic Flavors. You'll fall in love with the food and ambiance of our little Oasis.</p>
+      </TextContainer>
+      <InfoImages>
+        <Thumbnail 
+          image={friedRice} 
+          alt='rice plate'
+          />
           <Thumbnail 
-            image={friedRice} 
-            alt='rice-image'
-            />
-            <Thumbnail 
-              image={signatureDrink} 
-              alt='drink-image'
-            />
-        </InfoImages>
-      </InfoContainer>
+            image={mojito} 
+            alt='mojito drink'
+          />
+      </InfoImages>
       <ImageContainer>
         <Thumbnail 
           image={bar} 
@@ -36,41 +33,29 @@ const EatDrinkSection = () => {
           info='Taste Reimagined'
           />
       </ImageContainer>
-    </Container>
+    </TwoColCaptionImage>
   );
 };
 
 export default EatDrinkSection;
 
-const Container = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  /* grid-template-rows: auto; */
-  grid-gap: 2rem;
-  height: 50%;
-`;
-
-const InfoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding-top: 50px;
-  height: 30%;
-  button {
-    margin-top: 40px;
-  }
-`;
-
 const TextContainer = styled.div`
+  align-self: flex-end;
 `;
 
 const InfoImages = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  /* justify-content: center; */
+  align-self: center;
   grid-gap: 1rem;
 `;
 
 const ImageContainer = styled.div`
-  /* max-height: 80%; */
   height: auto;
+  grid-column: 2 / 3;
+  grid-row: 1 / 3;
+  @media (max-width: 1099px) {
+    grid-column: 1 / 2;
+    grid-row: 3 / 4;
+  }
 `;
