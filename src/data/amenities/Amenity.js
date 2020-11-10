d597b4d8df969fb44a9d5a73f55f34750dac238f
asabogal <asabogal.git@gmail.com>
2020-11-10 18:25:21 -0500

    add ammenities descriptions

--- src/data/amenities/Amenity.js
+++ src/data/amenities/Amenity.js
@@ -1,58 +1,48 @@
 export const amenities = [
   {
     heading: 'Our Garden',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    description: 'Our hidden gem, the heart of the hotel. Read a good book, endulge with a glass of rose, and simply relax on one of the multiple outdoor sofas and sitting areas. In the evenings, our in-house restaurant and bar serves cocktails and dinner.',
     link: '/amenities/garden'
   },
   {
-    heading: 'Our Lobby',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    link: '/amenities/lobby'
-  },
-  {
     heading: 'Art Gallery',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    description: 'The Hotel’s namesake Gallery features works on display from local and international artists. Every work of art is available for purchase, and the Gallery Hotel works with its artists to continuously refresh our offerings as well as organize events from time to time to showcase particular artists.',
     link: '/amenities/gallery'
   },
   {
     heading: 'Reception and Game Room',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    description: 'Enjoy a competitive backgammon or chess match with a friend in our sumptuous game room.',
     link: '/amenities/reception'
   },
   {
     heading: 'Movie and Media Room',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    description: 'While watching a movie while cuddled up in your bedroom may be a personal favorite, the hotel’s 10 person Movie and Media room features a massive projection display for a more exhilarating watching experience.',
     link: '/amenities/media'
   },
   {
     heading: 'Rooms',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    link: '/rooms'
-  },
-  {
-    heading: 'Suites',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    description: 'The Gallery offers 8 guest rooms and 2 suites, each of which is uniquely decorated. And though no two accommodations are the same, guests rave about our full range of room amenities, which include complementary high-speed internet access, flat-screen TVs, hair dryers, and fully stocked food and beverage minibars.',
     link: '/rooms'
   },
   {
     heading: 'In-House Restaurant',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    description: 'The Gallery Hotel’s in-house restaurant KonTiki features a dining experience unlike any other in the region. Award winning food and an extensive and meticulously designed cocktail menu will ensure a memorable experience for all guests. ',
     link: '/eatdrink'
   },
   {
     heading: 'High Speed Wi-Fi',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    description: 'Up to 400Mbps of internet speed troughout the property.',
   },
   {
     heading: 'Daily Housekeeping',
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    description: "Fresh towels and general room upkeep daily. "
   },
   {
     heading: 'Continental Breakfast',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    description: 'Daily continental breakfast included with the weekend room rates. Healthy and essential assorted items perfect to start your day as you relax with a delicious Espresso on our sunny Garden. *COVID-19 restrictions apply',
   },
   {
     heading: 'Private Parking',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    description: 'Complimentary on-site parking available on a first come, first served basis. Additional parking available at our secondary parking lot across the street.',
   },
 ]
