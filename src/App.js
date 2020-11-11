dc09fb642db1996e9124ae5fc8203f4324f89e27
asabogal <asabogal.git@gmail.com>
2020-11-11 15:14:19 -0500

    add react-helmet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Helmet from 'react-helmet';
 import Layout from './components/hoc/Layout';
 import HomePage from './pages/HomePage';
 import Location from './pages/Location';
@@ -17,24 +18,32 @@ import RedirectRoute from './routing/RedirectRoute';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Switch>
-            <Route exact path='/' component={HomePage}/>
-            <Route exact path='/location' component={Location}/>
-            <Route exact path='/amenities' component={Amenities}/>
-            <Route exact path= '/amenities/:name' render={props => <AmenitiesGallery {...props}/>}/>
-            <Route exact path='/rooms' component={Rooms}/>
-            <Route exact path= '/rooms/:name' render={props => <RoomPage {...props}/>}/>
-            <Route exact path='/eatdrink' component={EatAndDrink}/>
-            <Route exact path= '/eatdrink/:name' render={props => <Menu {...props}/>}/>
-            <Route exact path='/events_blocks' component={EventsBlocks}/>
-            <Route exact path='/contact' component={ContactUs}/>
-            <Route exact path='/policies' component={Policies}/>
-            <RedirectRoute path='*'/>
-          </Switch>
-      </Layout>
-    </BrowserRouter>
+    <>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>The Gallery Hotel</title>  
+      <meta name="description" content="The Gallery Hotel - A boutique hotel in Greenport, New York"/>
+      <meta name="keywords" content="Gallery Hotel, Gallery, Hotel, The Gallery Hotel, Greenport, Greenport Hotel, greenport hotel, hotel, gallery, long island, greenport new york hotel" />  
+    </Helmet>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+              <Route exact path='/' component={HomePage}/>
+              <Route exact path='/location' component={Location}/>
+              <Route exact path='/amenities' component={Amenities}/>
+              <Route exact path= '/amenities/:name' render={props => <AmenitiesGallery {...props}/>}/>
+              <Route exact path='/rooms' component={Rooms}/>
+              <Route exact path= '/rooms/:name' render={props => <RoomPage {...props}/>}/>
+              <Route exact path='/eatdrink' component={EatAndDrink}/>
+              <Route exact path= '/eatdrink/:name' render={props => <Menu {...props}/>}/>
+              <Route exact path='/events_blocks' component={EventsBlocks}/>
+              <Route exact path='/contact' component={ContactUs}/>
+              <Route exact path='/policies' component={Policies}/>
+              <RedirectRoute path='*'/>
+            </Switch>
+        </Layout>
+      </BrowserRouter>
+    </>
   );
 }
 
