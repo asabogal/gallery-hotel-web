27ea976fc761497ab9b57fbf51ed3836be5d0f2e
asabogal <asabogal.git@gmail.com>
2020-11-11 14:41:42 -0500

    reset homePage Route to /

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
     <BrowserRouter>
       <Layout>
         <Switch>
-            <Route exact path='/gallery-hotel-web' component={HomePage}/>
+            <Route exact path='/' component={HomePage}/>
             <Route exact path='/location' component={Location}/>
             <Route exact path='/amenities' component={Amenities}/>
             <Route exact path= '/amenities/:name' render={props => <AmenitiesGallery {...props}/>}/>
